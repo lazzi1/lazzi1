05abb850804e60bfe9c0e089f620885dfef63e81
lazzi1 <matricpass2021@gmail.com>
2024-12-15 12:50:31 +0530

    Add files via upload

---
+++ nonRepeatingChar.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char nonRepeatingChar(char s[]) {
+    
+    int length=strlen(s);
+    
+    for(int i=0;i<length;i++)
+    {
+        int j;
+        for(j=0;j<length;j++)
+        {
+            if(i!=j&&s[i]==s[j])
+            {
+                break;
+            }
+        }
+        
+        if(j==length)
+        {
+            return s[i];
+        }
+    }
+    
+    return '$' ;
+}
+
+
+int main() {
+    char s[20];
+    
+    //scanf("%s",s);
+    fgets(s, sizeof(s), stdin);
+    
+        char result = nonRepeatingChar(s);
+        if (result == '$') {
+            printf("%d\n", -1);
+        } else {
+            printf("%c\n", result);
+        }
+    
+
+    return 0;
+}
